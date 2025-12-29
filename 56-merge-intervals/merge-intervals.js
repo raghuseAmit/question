d0c528866ba9d435196679647a5ebac1b7abea4c
Amit Raghuse <amitraghuse11@gmail.com>
2025-12-29 23:34:42 +0530

    Time: 9 ms (53.90%) | Memory: 66.3 MB (22.20%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -7,7 +7,7 @@ var merge = function(intervals) {
     intervals.sort((a, b) => a[0]-b[0] || a[1]-b[1]);
     let merged = [];
     for(let i = 0; i < n; i++) {
-        const [currStart, currEnd] = intervals[i]
+        const [currStart, currEnd] = intervals[i];
         if (merged.length === 0 || merged[merged.length - 1][1] < currStart) {
             merged.push([currStart, currEnd]);
         } 
