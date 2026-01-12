8f404f58237a2d65bfbe6ae5a544cca819983da5
Amit Raghuse <amitraghuse11@gmail.com>
2026-01-12 23:11:39 +0530

    Time: 10 ms (13.36%) | Memory: 46.9 MB (5.67%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -3,9 +3,7 @@ class Solution {
         StringBuilder res = new StringBuilder();
         Stack<Integer> stack = new Stack<>();
         Set<Integer> removeIndices = new HashSet<>();
-        
-        // First pass: find indices to remove using stack
-        for (int i = 0; i < s.length(); i++) {
+            for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
             if (c == '(') {
                 stack.push(i);
@@ -17,8 +15,6 @@ class Solution {
                 }
             }
         }
-        
-        // Second pass: build result skipping remove indices
         for (int i = 0; i < s.length(); i++) {
             if (!removeIndices.contains(i)) {
                 res.append(s.charAt(i));
