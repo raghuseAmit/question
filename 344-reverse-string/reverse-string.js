6a131bcbbbd76cbee2aa520a63dc09e7efa6af08
Amit Raghuse <amitraghuse11@gmail.com>
2026-04-05 23:17:12 +0530

    Time: 4 ms (9.24%) | Memory: 62.8 MB (11.03%) - LeetSync

---
+++ 344-reverse-string/reverse-string.js
@@ -0,0 +1,8 @@
+function reverseString(s) {
+    let left = 0, right = s.length - 1;
+    while (left < right) {
+        [s[left], s[right]] = [s[right], s[left]];
+        left++;
+        right--;
+    }
+}
