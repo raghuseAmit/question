07582d2305f70bf4be2e9130615b8f84ffab25e9
Amit Raghuse <amitraghuse11@gmail.com>
2026-04-11 00:19:05 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (73.00%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -2,13 +2,9 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
 
         int low = 0, high = nums.length - 1;
-
-        // If target is greater than all elements
         if (target > nums[nums.length - 1]) {
             return nums.length;
         }
-
-        // If target is smaller than or equal to first element
         if (target <= nums[0]) {
             return 0;
         }
@@ -24,8 +20,6 @@ class Solution {
                 low = mid + 1;
             }
         }
-
-        // low is the correct insertion position
         return low;
     }
 }
